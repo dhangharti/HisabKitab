a2469acec5531ffd2bc0858e2e7d94aa5cdb01ac
dhangharti <105736566+dhangharti@users.noreply.github.com>
2025-10-22 13:49:49 +0545

    Add files via upload

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9464494046-3c19d",
+  "appId": "1:63519478087:web:cde73fad2c8ab6dcaf3c62",
+  "apiKey": "AIzaSyDF9vfzRvFuPcCqwGEDSTyl53nBvp8RJ0M",
+  "authDomain": "studio-9464494046-3c19d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "63519478087"
+};
