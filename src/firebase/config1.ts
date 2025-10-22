cd10a67612d8c5ce25d10b8e039ca00c06cc9e57
dhangharti <105736566+dhangharti@users.noreply.github.com>
2025-10-22 22:04:29 +0545

    Create config1.ts

---
+++ src/firebase/config1.ts
@@ -0,0 +1,16 @@
+
+
+'use client';
+
+// This file contains the public, client-side configuration for your Firebase project.
+// This object is used to initialize the Firebase App. It is safe to expose this data
+// in a client-side context. Access to your Firebase resources is controlled by
+// your Firestore Security Rules, not by the presence of this config object.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6OEYM3z3CfaB0_67x6l5gZ5yHOUXaKyM",
+  authDomain: "studio-9464494046-3c19d.firebaseapp.com",
+  projectId: "studio-9464494046-3c19d",
+  appId: "1:63519478087:web:cde73fad2c8ab6dcaf3c62",
+  messagingSenderId: "63519478087"
+};
