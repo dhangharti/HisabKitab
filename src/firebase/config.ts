2a3b241bb79350592a559f5916576bd5b2040044
dhangharti <105736566+dhangharti@users.noreply.github.com>
2025-10-22 17:14:50 +0545

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
-  "projectId": "studio-9464494046-3c19d",
-  "appId": "1:63519478087:web:cde73fad2c8ab6dcaf3c62",
-  "apiKey": "AIzaSyDF9vfzRvFuPcCqwGEDSTyl53nBvp8RJ0M",
-  "authDomain": "studio-9464494046-3c19d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "63519478087"
+  apiKey: "AIzaSyC6OEYM3z3CfaB0_67x6l5gZ5yHOUXaKyM",
+  authDomain: "studio-9464494046-3c19d.firebaseapp.com",
+  projectId: "studio-9464494046-3c19d",
+  appId: "1:63519478087:web:cde73fad2c8ab6dcaf3c62",
+  messagingSenderId: "63519478087"
 };
