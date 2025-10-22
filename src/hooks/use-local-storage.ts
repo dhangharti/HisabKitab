fdff13d8c484c219817ab0951d64f2a9eeed4f74
dhangharti <105736566+dhangharti@users.noreply.github.com>
2025-10-22 13:47:36 +0545

    Add files via upload

---
+++ src/hooks/use-local-storage.ts
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+
+function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void, () => void] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+     try {
+      const item = window.localStorage.getItem(key);
+      setStoredValue(item ? JSON.parse(item) : initialValue);
+    } catch (error) {
+      console.error(error);
+      setStoredValue(initialValue);
+    }
+  }, [key, initialValue]);
+
+  const setValue = (value: T) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const clearValue = () => {
+    try {
+        if (typeof window !== 'undefined') {
+            window.localStorage.removeItem(key);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
+  return [storedValue, setValue, clearValue];
+}
+
+export default useLocalStorage;
